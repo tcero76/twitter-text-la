10b478e7f6f3c9fac92cf3b0397f0737e12e2a6f
Leonardo Lastra <leonardo.lastra@gmail.com>
2024-10-25 20:04:48 -0300

    agrega github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "twitter-text-la",
   server: {
     host: true,
     port: 5173, // This is the port which we will use in docker
