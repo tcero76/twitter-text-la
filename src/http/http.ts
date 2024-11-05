b4c1e16ce976e15d39b07837fa6f5964d9b836e5
Leonardo Lastra <leonardo.lastra@gmail.com>
2024-11-05 16:41:49 -0300

    negar perfil de desarrollo

--- src/http/http.ts
+++ src/http/http.ts
@@ -2,7 +2,7 @@ import axios, { AxiosResponse } from 'axios'
 import { suggestionList } from '../utils/mockHttp'
 
 export function getSuggestion():Promise<AxiosResponse<Array<string>>> {
-    if (import.meta.env.VITE_APP_PROFILE==='DEV') {
+    if (!import.meta.env.DEV) {
         return Promise.resolve(suggestionList)
     }
     return axios.get<Array<string>>('/api/v1/suggestions')
